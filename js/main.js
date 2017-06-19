4c136fbc6807d3ee7754a9dc0ce373d9913dd0b5
Viv <Viv@Viviennes-MacBook-Pro.local>
2017-06-19 08:07:05 -0400

    Adds javascript click event for previous test
    Changes box colour on click with Vanilla JS.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-// Pure Javascript selector: document.getElementByClassName('box') //
+// VANILLA JAVASCRIPT //
+var boxes = document.getElementsByClassName('box');
+var box = boxes[0];
 
+box.addEventListener("click", function(){
+	if(box.classList.contains('js-active')){
+ 		// js-active is in the list
+    box.classList.remove('js-active');
+  } else {
+  	// js-active is NOT in the list
+    box.classList.add('js-active');
+  }
+});
+
+
+/* JQUERY
 var $box = $('.box');
 
 $box.on('click', function () {
 	$box.toggleClass('js-active');
 })
+*/
