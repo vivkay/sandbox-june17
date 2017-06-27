a12bb983ea5e9ee0171e3b1a4c9714d8731dfdc8
Viv <Viv@Viviennes-MacBook-Pro.local>
2017-06-26 22:25:28 -0400

    Adds click event for sprite
    adds variable sprite and sandwich, with click function.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//TEST: window.alert("hello");
+
 // VANILLA JAVASCRIPT //
 var boxes = document.getElementsByClassName('box');
 var box = boxes[0];
@@ -13,10 +15,45 @@ box.addEventListener("click", function(){
 });
 
 
-/* JQUERY
+/* JQUERY for above ^ 
 var $box = $('.box');
 
 $box.on('click', function () {
 	$box.toggleClass('js-active');
 })
+*/
+
+/*
+var frames = document.getElementsByClassName('sandwich');
+var frame = frames[0];
+
+frame.addEventListener("click", function(){
+	add(box.classList('full')){
+};
+*/
+
+//VANILLA JAVASCRIPT //
+
+var sprite = document.getElementsByClassName('sandwich');
+var sandwich = sprite[0];
+
+sandwich.addEventListener("click", function(){
+		sandwich.classList.add('bite1');
+});
+
+/* FAILED ATTEMPT :( : 
+sandwich.addEventListener("click", function(){
+	if(sandwich.classList.contains('full')){
+		sandwich.classList.remove('full');
+		sandwich.classList.add('bite1');
+	}
+	else if(sandwich.classList.contains('bite1')){
+		sandwich.classList.remove('bite1');
+		sandwich.classList.add('bite2');
+	}
+	else if(sandwich.classList.contains('bite2')){
+		sandwich.classList.remove('bite2');
+		sandwich.classList.add('bite3');
+	}
+});
 */
